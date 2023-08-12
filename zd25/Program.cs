8da525762315761f2bdced95e66d6ceaf2228f24
Mikebulatov <mikebulatov@yandex.ru>
2023-08-12 22:27:06 +0700

    Задача 27

--- zd25/Program.cs
+++ zd25/Program.cs
@@ -1,20 +1,20 @@
 ﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-int Pow(int A,B)
-{// фнукция возво дит число А в степень B.
-int st=1;
-while (B > 0 )
-{
-    st=st*a;
-    B--;
-}
-return st;
+int Power(int A,int B)
+{// фнукция возводит число А в степень B.
+    int st=1;
+    while (B > 0 )
+        {
+        st=st*A;
+        B--;
+        }
+    return st;
 }
 
 Console.WriteLine("Задача 25: Программа, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
 Console.Write("Введите чиссло А:");
-int A=Convert.ToInt32(Console.ReadLine());
+int a=Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите чиссло B:");
-int A=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"Число A в степени B равно:{power(A,B)}");
+Console.WriteLine($"Число {a} в степени {b} равно:{Power(a,b)}");
---
+++ zd27/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SummNum(int Num)
+{// фнукция возвращает сумму цифр числа.
+    int st=0;
+    while (Num > 0 )
+        {
+        st=st+Num%10;
+        Num=Num/10;
+        }
+    return st;
+}
+
+Console.WriteLine("Задача 27.Программа, которая принимает на вход число и выдаёт сумму цифр в числе.");
+Console.Write("Введите число:");
+int a=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"сумма цифр сисла {a} равна:{SummNum(a)}");
