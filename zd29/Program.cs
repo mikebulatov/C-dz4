bcfcdbaeb0c94d4596395c303e845c2898427bbf
Mikebulatov <mikebulatov@yandex.ru>
2023-08-12 22:44:44 +0700

    Задача 29.

---
+++ zd29/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.WriteLine("Задача 27.Программа, задаёт массив из 8 элементов и выводит их на экран");
+int [] mass=new int [8];
+
+void CrateArray()
+{
+for(int i=0; i<mass.Length; i++)
+    {
+        mass[i]= new Random().Next(0,999);
+        }
+}
+//_________________________________________________________
+
+
+CrateArray();
+Console.Write("Сформированный массив:");
+for(int i=0; i<mass.Length; i++)
+    {
+                Console.Write(mass[i]);
+        Console.Write(" ");
+    }
+Console.WriteLine();
